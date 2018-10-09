4a2fc58eb3ae663ac7b00dca775f74415d8ee2a2
saber2pr <saber2pr@gmail.com>
2018-10-09 17:35:12 +0800

    update

--- src/common/js/config.js
+++
@@ -1,9 +0,0 @@
-const url = document.referrer
-const pathName = url.pathName
-const pos = url.indexOf(pathName)
-const root = url.substring(0, pos)
-
-/* eslint-disable */
-export const home = root + '/home.json'
-export const project = root + '/project.json'
-export const about = root + '/about.json'
